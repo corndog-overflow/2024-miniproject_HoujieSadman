f61e527dcba45820be6e25d3ed2505be08624d7e
= <kabirs@bu.edu>
2024-09-10 15:51:30 -0400

    added music

--- assignment/exercise_light.py
+++ assignment/exercise_light.py
@@ -14,8 +14,8 @@ adc = machine.ADC(ADC2)
 
 blink_period = 0.1
 
-max_bright = 20000
-min_bright = 10000
+max_bright = 50000
+min_bright = 13000
 
 
 def clip(value: float) -> float:
@@ -36,8 +36,8 @@ while True:
     So we use function clip()
     """
 
-    duty_cycle = clip((value - min_bright) / (max_bright - min_bright))
-
+    duty_cycle = clip((value - max_bright) / (min_bright - max_bright))
+    print("duty cycle:" + str(duty_cycle) + "\n")
     led.high()
     time.sleep(blink_period * duty_cycle)
 
--- assignment/exercise_sound.py
+++ assignment/exercise_sound.py
@@ -11,29 +11,57 @@ import utime
 # GP16 is the speaker pin
 SPEAKER_PIN = 16
 
-# create a Pulse Width Modulation Object on this pin
+# Create a Pulse Width Modulation Object on this pin
 speaker = machine.PWM(machine.Pin(SPEAKER_PIN))
 
 
 def playtone(frequency: float, duration: float) -> None:
-    speaker.duty_u16(1000)
-    speaker.freq(frequency)
-    utime.sleep(duration)
+    """Play a tone with a specific frequency and duration."""
+    speaker.duty_u16(1000)  # Set the PWM duty cycle (volume)
+    speaker.freq(int(frequency))  # Set the frequency of the tone
+    utime.sleep(duration)  # Play the tone for the given duration
 
 
-def quiet():
-    speaker.duty_u16(0)
+def quiet() -> None:
+    """Stop the tone."""
+    speaker.duty_u16(0)  # Set the duty cycle to 0 to turn off the tone
 
 
-freq: float = 30
-duration: float = 0.1  # seconds
+# Define notes and their frequencies
+notes = {
+    "1":494,
+    "2":440,
+    "wait" : 0,
+    "3": 783/2,
+    "4": 739/2,
+    "5": 659/2,
+    "6": 311,
+    "7": 311,
+    "8": 246,
+    "9": 220
+}
 
-print("Playing frequency (Hz):")
+song = [
+    ("1", 0.185),
+    ("2", 0.185),
+    ("1", 1.5),
+    ("wait", 0),
+    ("2", 0.185),
+    ("3", 0.185),
+    ("4", 0.185),
+    ("5", 0.185),
+    ("6", 0.185*3),
+    ("5", 0.185*2)
+    
+]
 
-for i in range(64):
-    print(freq)
-    playtone(freq, duration)
-    freq = int(freq * 1.1)
+for note, duration in song:
+    frequency = notes.get(note, 0)
+    if frequency >= 0:
+        if frequency == 0:
+            quiet()
+        else:
+            playtone(int(frequency*1.1), duration)
+    utime.sleep(0.05)  
 
-# Turn off the PWM
 quiet()
