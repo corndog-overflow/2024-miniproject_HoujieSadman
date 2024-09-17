625fa9f46ef1e976018516c9e12dac2b2287f19a
= <kabirs@bu.edu>
2024-09-17 16:33:25 -0400

    added score listing

--- react-app/my-app/src/App.js
+++ react-app/my-app/src/App.js
@@ -15,25 +15,31 @@ import { getAnalytics } from "firebase/analytics";
 const secret_key = "SECRET_KEY"
 
 function fetchData() {
-  fetch(`https://api.thingspeak.com/channels/2657449/fields/1.json?api_key=${secret_key}&results=3`)
+  fetch(`https://api.thingspeak.com/channels/2657449/fields/1.json?api_key=${secret_key}&results=4`)
     .then(res_fast => res_fast.json())
     .then(data => {
       console.log(data["feeds"][0]);
       document.getElementById("fast").innerText = data["feeds"][0]["field1"];
     });
 
-  fetch(`https://api.thingspeak.com/channels/2657449/fields/2.json?api_key=${secret_key}&results=3`)
+  fetch(`https://api.thingspeak.com/channels/2657449/fields/2.json?api_key=${secret_key}&results=4`)
     .then(res_fast => res_fast.json())
     .then(data => {
       console.log(data["feeds"][1]);
       document.getElementById("slow").innerText = data["feeds"][1]["field2"];
     });
 
-  fetch(`https://api.thingspeak.com/channels/2657449/fields/3.json?api_key=${secret_key}&results=3`)
+  fetch(`https://api.thingspeak.com/channels/2657449/fields/3.json?api_key=${secret_key}&results=4`)
     .then(res_fast => res_fast.json())
     .then(data => {
       console.log(data["feeds"][2]);
       document.getElementById("avg").innerText = data["feeds"][2]["field3"];
+    });
+    fetch(`https://api.thingspeak.com/channels/2657449/fields/4.json?api_key=${secret_key}&results=4`)
+    .then(res_fast => res_fast.json())
+    .then(data => {
+      console.log(data["feeds"][3]);
+      document.getElementById("score").innerText = data["feeds"][3]["field4"];
     });
 }
 
@@ -55,6 +61,7 @@ function App() {
           Fastest Time: <span id = "fast">[NO DATA FOUND]</span>ms <br></br>
           Slowest Time: <span id = "slow">[NO DATA FOUND]</span>ms <br></br>
           Average Time: <span id = "avg">[NO DATA FOUND]</span>ms <br></br>
+          Score: <span id = "score">[NO DATA FOUND]</span>ms <br></br>
         </p>
       </header>
       
