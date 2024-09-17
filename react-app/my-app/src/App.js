1513af6e90176c4a22344587cbbcbd9b30b85352
= <kabirs@bu.edu>
2024-09-17 17:44:27 -0400

    edited html

--- react-app/my-app/src/App.js
+++ react-app/my-app/src/App.js
@@ -16,21 +16,21 @@ const secret_key = "MBHJQKMNNFV2L7F3"
 
 
 function fetchData() {
-  fetch(`https://api.thingspeak.com/channels/2657449/fields/1.json?api_key=${secret_key}&results=3`)
+  fetch(`https://api.thingspeak.com/channels/2657449/fields/1.json?api_key=${secret_key}&results=4`)
     .then(res_fast => res_fast.json())
     .then(data => {
       console.log(data["feeds"][0]);
       document.getElementById("fast").innerText = data["feeds"][0]["field1"];
     });
 
-  fetch(`https://api.thingspeak.com/channels/2657449/fields/2.json?api_key=${secret_key}&results=3`)
+  fetch(`https://api.thingspeak.com/channels/2657449/fields/2.json?api_key=${secret_key}&results=4`)
     .then(res_fast => res_fast.json())
     .then(data => {
       console.log(data["feeds"][1]);
       document.getElementById("slow").innerText = data["feeds"][1]["field2"];
     });
 
-  fetch(`https://api.thingspeak.com/channels/2657449/fields/3.json?api_key=${secret_key}&results=3`)
+  fetch(`https://api.thingspeak.com/channels/2657449/fields/3.json?api_key=${secret_key}&results=4`)
     .then(res_fast => res_fast.json())
     .then(data => {
       console.log(data["feeds"][2]);
@@ -43,6 +43,8 @@ function fetchData() {
       document.getElementById("score").innerText = data["feeds"][3]["field4"];
     });
 }
+
+
 
 // setInterval(fetchData, 5000);
 
@@ -62,7 +64,7 @@ function App() {
           Fastest Time: <span id = "fast">[NO DATA FOUND]</span>ms <br></br>
           Slowest Time: <span id = "slow">[NO DATA FOUND]</span>ms <br></br>
           Average Time: <span id = "avg">[NO DATA FOUND]</span>ms <br></br>
-          Score: <span id = "score">[NO DATA FOUND]</span>ms <br></br>
+          Score: <span id = "score">[NO DATA FOUND]</span><br></br>
         </p>
       </header>
       
